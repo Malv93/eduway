87e31025354bd7d36019549b541de34918bf4c66
Marco Malvicini <marco.malvicini31@gmail.com>
2020-12-28 09:17:09 +0100

    Add mapping elements for team and methodology sections. Add padding bottom for heading section.

--- components/homeSections/methodology.js
+++ components/homeSections/methodology.js
@@ -2,6 +2,39 @@ import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 import styles from "../../styles/home.module.css";
 
+const methodologySteps = [
+  {
+    id: 1,
+    title: "Condividiamo nuovi obiettivi",
+    description:
+      "Lorem ipsum dolor sit amet, consectetu adipiscing elit, sed do eiusmod",
+  },
+  {
+    id: 2,
+    title: "Individuiamo problemi e punti di forza",
+    description:
+      "Lorem ipsum dolor sit amet, consectetu adipiscing elit, sed do eiusmod",
+  },
+  {
+    id: 3,
+    title: "Progettiamo l'accompagnamento",
+    description:
+      "Lorem ipsum dolor sit amet, consectetu adipiscing elit, sed do eiusmod",
+  },
+  {
+    id: 4,
+    title: "Lavoriamo per raggiungere gli obiettivi",
+    description:
+      "Lorem ipsum dolor sit amet, consectetu adipiscing elit, sed do eiusmod",
+  },
+  {
+    id: 5,
+    title: "Termine del progetto e definizione nuovi obiettivi",
+    description:
+      "Lorem ipsum dolor sit amet, consectetu adipiscing elit, sed do eiusmod",
+  },
+];
+
 export default function Methodology() {
   return (
     <Container
@@ -11,53 +44,16 @@ export default function Methodology() {
     >
       <h2 className="section-title">Il Metodo Eduway</h2>
       <ol>
-        <li className={styles.methodologyStep}>
-          <Card bg="warning" text="dark">
-            <Card.Header>Condividiamo nuovi obiettivi</Card.Header>
-            <Card.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod
-            </Card.Body>
-          </Card>
-        </li>
-        <li className={styles.methodologyStep}>
-          <Card bg="warning" text="dark">
-            <Card.Header>Individuiamo problemi e punti di forza</Card.Header>
-            <Card.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod
-            </Card.Body>
-          </Card>
-        </li>
-        <li className={styles.methodologyStep}>
-          <Card bg="warning" text="dark">
-            <Card.Header>Progettiamo l'accompagnamento</Card.Header>
-            <Card.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod
-            </Card.Body>
-          </Card>
-        </li>
-        <li className={styles.methodologyStep}>
-          <Card bg="warning" text="dark">
-            <Card.Header>Lavoriamo per raggiungere gli obiettivi</Card.Header>
-            <Card.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod
-            </Card.Body>
-          </Card>
-        </li>
-        <li className={styles.methodologyStep}>
-          <Card bg="warning" text="dark">
-            <Card.Header>
-              Termine del progetto e definizione nuovi obiettivi
-            </Card.Header>
-            <Card.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod
-            </Card.Body>
-          </Card>
-        </li>
+        {methodologySteps.map((step) => {
+          return (
+            <li key={step.id} className={styles.methodologyStep}>
+              <Card bg="warning" text="dark">
+                <Card.Header>{step.title}</Card.Header>
+                <Card.Body>{step.description}</Card.Body>
+              </Card>
+            </li>
+          );
+        })}
       </ol>
     </Container>
   );
--- components/homeSections/team.js
+++ components/homeSections/team.js
@@ -2,31 +2,68 @@ import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 import styles from "../../styles/home.module.css";
 
+const founders = [
+  {
+    id: 1,
+    name: "Andrea Dasti",
+  },
+  {
+    id: 2,
+    name: "Stefano Lepre",
+  },
+];
+
+const collaborators = [
+  {
+    id: 1,
+    name: "Emanuele Bergami",
+    role: "Formatore",
+  },
+  {
+    id: 2,
+    name: "Heidi Corti",
+    role: "Responsabile Grafica",
+  },
+  {
+    id: 3,
+    name: "Marco Malvicini",
+    role: "Responsabile IT",
+  },
+];
+
 export default function Team() {
   return (
     <Container fluid id="team" className={`section ${styles.teamSection}`}>
       <h2 className="section-title">Team</h2>
-      <Card bg="primary" text="white" className={styles.teamCard}>
-        <Card.Header>Andrea Dasti</Card.Header>
-        <Card.Body>Co-Founder</Card.Body>
-      </Card>
-      <Card bg="primary" text="white" className={styles.teamCard}>
-        <Card.Header>Stefano Lepre</Card.Header>
-        <Card.Body>Co-Founder</Card.Body>
-      </Card>
+      {founders.map((founder) => {
+        return (
+          <Card
+            key={founder.id}
+            bg="primary"
+            text="white"
+            className={styles.teamCard}
+          >
+            <Card.Header>{founder.name}</Card.Header>
+            <Card.Body>Co-Founder</Card.Body>
+          </Card>
+        );
+      })}
+
       <h3 className="title">Collaboratori</h3>
-      <Card bg="light" text="dark" border="primary" className={styles.teamCard}>
-        <Card.Header>Emanuele Bergami</Card.Header>
-        <Card.Body>Formatore</Card.Body>
-      </Card>
-      <Card bg="light" text="dark" border="primary" className={styles.teamCard}>
-        <Card.Header>Heidi Corti</Card.Header>
-        <Card.Body>Responsabile Grafica</Card.Body>
-      </Card>
-      <Card bg="light" text="dark" border="primary" className={styles.teamCard}>
-        <Card.Header>Marco Malvicini</Card.Header>
-        <Card.Body>Responsabile IT</Card.Body>
-      </Card>
+      {collaborators.map((collaborator) => {
+        return (
+          <Card
+            key={collaborator.id}
+            bg="light"
+            text="dark"
+            border="primary"
+            className={styles.teamCard}
+          >
+            <Card.Header>{collaborator.name}</Card.Header>
+            <Card.Body>{collaborator.role}</Card.Body>
+          </Card>
+        );
+      })}
     </Container>
   );
 }
